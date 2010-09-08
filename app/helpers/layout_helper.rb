a9e7b7f5b2fb1d4e6dcf45153e01cc1410723af9
Andrea <andrea.damico@gmail.com>
2010-09-08 10:06:10 +0200

    added link_to_logo helper, testing layout links

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -3,6 +3,11 @@
 # to do so you may need to add this line to your ApplicationController
 #   helper :layout
 module LayoutHelper
+  def link_to_logo(path=root_path, image_src="logo.png", alt="Sample App", css_class="round")
+    link_to(path) do
+      image_tag(image_src, :alt => alt, :class => css_class)
+    end
+  end
   def title(page_title, show_title = true)
     base_title = "Ruby on Rails Tutorial Sample App"
     page_title = @title.nil? ? base_title : base_title + " | " + @title
