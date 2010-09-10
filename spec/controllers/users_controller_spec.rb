804141edb67a5ca27d80e1322d8e6462006be5ec
Andrea <andrea.damico@gmail.com>
2010-09-10 11:41:35 +0200

    update and list users are done

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -93,4 +93,147 @@ describe UsersController do
     end
   end
 
+  describe "GET 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+      get :edit, :id => @user
+    end
+
+    it "should be successful" do
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Edit user")
+    end
+
+    it "should have a link to change the Gravatar" do
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url, :content => "change")
+    end
+  end
+
+  describe "PUT 'update'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    context "failure" do
+      before(:each) do
+        put :update, :id => @user, :user => { 
+          :name => "", 
+          :email => "", 
+          :password => "", 
+          :password_confirmation => ""}
+      end
+
+      it "should render the 'edit' page" do
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title", :content => "Edit user")
+      end
+    end
+
+    context "success" do
+      before(:each) do
+        put :update, :id => @user, :user => {
+          :name => "New User",
+          :email => @user.email,
+          :password => @user.password,
+          :password_confirmation => @user.password }
+      end
+
+      it "should change the user's attributes" do
+        user = assigns(:user)
+        @user.reload
+        @user.name.should == user.name
+      end
+
+      it "should redirect to the user show page" do
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a flash message" do
+        flash[:success].should =~ /updated/i
+      end
+    end
+  end
+
+  describe "authentication of edit/update pages" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    context "for guest users" do
+      it "should deny access to 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+      end
+      it "should deny access to 'update'" do
+        put :update, :id => @user
+        response.should redirect_to(signin_path)
+      end
+      it "should have a 'please sign in' flash" do
+        get :edit, :id => @user
+        flash[:notice].should =~ /please sign in/i
+      end
+    end
+
+    context "for signed_in users" do
+      
+      before(:each) do
+        wrong_user = Factory(:user, :email => "user@example.net")
+        test_sign_in(wrong_user)
+      end
+
+      it "should require matching users for 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(root_path)
+      end
+      it "should require matching users for 'update'" do
+        put :update, :id => @user
+        response.should redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "GET 'index'" do
+    
+    context "for non-signed_in users" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to(signin_path)
+        flash[:notice].should =~ /sign in/i
+      end
+    end
+
+    context "for signed-in users" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        second = Factory(:user)
+        third = Factory(:user)
+        @users = [@user, second, third]
+        get :index
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title", :content => "All users")
+      end
+
+     it "should have an element for each user" do
+        @users.each do |user|
+          response.should have_selector("li", :content => user.name)
+        end
+      end 
+    end
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |f|
-  f.name                  "Andrea D'Amico"
-  f.email                 "andrea@ada.com"
+  f.sequence(:name) {|n| "user#{n}"}
+  f.email {|user| "#{user.name}@example.com".downcase}
   f.password              "foobar"
   f.password_confirmation "foobar"
 end
